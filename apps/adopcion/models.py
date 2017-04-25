610c76d538ce144b58fc6f9fa4edec0ec5aeaf72
adamagi <marc.gimeno@adamo.es>
2017-04-25 17:04:42 +0200

    a
    a

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    nombre = models.Charfield(max_lengh=50)
-    apellidos = models.Charfield(max_lengh=70)
+    nombre = models.CharField(max_lengh=50)
+    apellidos = models.CharField(max_lengh=70)
     edad = models.IntegerField()
-    telefono = models.Charfield(max_lengh=12)
+    telefono = models.CharField(max_lengh=12)
     email = models.EmailField()
     domicilio = models.TextField()
