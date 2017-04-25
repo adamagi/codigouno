ba83fd17657a4c4151cb776fb5e59559457ca96b
adamagi <marc.gimeno@adamo.es>
2017-04-25 17:01:14 +0200

    a
    a

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre = models.Charfield(max_lengh=50)
+    apellidos = models.Charfield(max_lengh=70)
+    edad = models.IntegerField()
+    telefono = models.Charfield(max_lengh=12)
+    email = models.EmailField()
+    domicilio = models.TextField()
--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Mascota(models.model):
+    nombre = models.Charfield(max_lengh=50)
+    sexo = models.Charfield(max_lengh=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
+
+        
