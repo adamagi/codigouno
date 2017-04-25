4a9f8b23b729d5d8e0523f1e9d1899c567b87bf9
adamagi <marc.gimeno@adamo.es>
2017-04-25 17:08:28 +0200

    a
    a

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Mascota(models.model):
+class Mascota(models.Model):
     folio = models.CharField(max_length=10, primary_key=True)
     nombre = models.CharField(max_length=50)
     sexo = models.CharField(max_length=10)
