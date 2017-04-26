6aafcd4623abf043d192d0d5b72829077345141a
adamagi <marc.gimeno@adamo.es>
2017-04-26 16:26:31 +0200

    a
    a

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -2,6 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from apps.adopcion.models import Persona
+
+
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+
 # Create your models here.
 class Mascota(models.Model):
     folio = models.CharField(max_length=10, primary_key=True)
@@ -9,5 +17,7 @@ class Mascota(models.Model):
     sexo = models.CharField(max_length=10)
     edad_aproximada = models.IntegerField()
     fecha_rescate = models.DateField()
+    persona = models.ForeignKey(Persona, null=True, blank= True, on_delete=models.CASCADE)
+    vacuna = models.ManytoManyField(Vacuna)
 
         
